8a63f35c3798012f22b368654b7602f4ad6a5fa6
Marek <marek.j.swakon@gmail.com>
2025-10-21 19:29:00 +0200

    Refactor main and motor controller logic for improved readability and performance

--- src/main.cpp
+++ src/main.cpp
@@ -15,94 +15,61 @@ int sbus_status;
 
 void setup()
 {
-    unsigned long last_time_us, now_us;
-    float dt;
-    int sbus_status;
+    Serial.begin(115200);
+    while (!Serial)
+        delay(10);
 
-    void setup()
+    MPU5060_Sensor.init();
+    SBUS_Reader.init();
+    Motor_Controller.init();
+
+    mode = DEFAULT_MODE;
+    if (mode == MODE_TANK)
     {
-        Serial.begin(115200);
-        while (!Serial)
-            delay(10);
-
-        MPU5060_Sensor.init();
-        SBUS_Reader.init();
-        Motor_Controller.init();
-
-        while (!Serial)
-            delay(10);
-
-        MPU5060_Sensor.init();
-        SBUS_Reader.init();
-        Motor_Controller.init();
-
-        mode = DEFAULT_MODE;
-        if (mode == MODE_TANK)
-        {
-            // change to tank
-        }
-        else
-        {
-            // change to copter
-        }
-
-        last_time_us = micros();
+        // change to tank
+    }
+    else
+    {
+        // change to copter
     }
 
-    void loop()
+    last_time_us = micros();
+}
+
+void loop()
+{
+    now_us = micros();
+    dt = (now_us - last_time_us) / 1000000.0f;
+    if (dt <= 0)
+        dt = 0.001f;
+    last_time_us = now_us;
+
+    MPU5060_Sensor.compute_orientation();
+
+    sbus_status = SBUS_Reader.Read_Sbus();
+    // TODO handle safe mode
+    switch (sbus_status)
     {
-        now_us = micros();
-        void loop()
-        {
-            now_us = micros();
-            dt = (now_us - last_time_us) / 1000000.0f;
-            if (dt <= 0)
-                dt = 0.001f;
-            if (dt <= 0)
-                dt = 0.001f;
-            last_time_us = now_us;
+    case 2:
+        Serial.println("Failsafe active");
+        return;
+        break;
+    case 1:
+        Serial.println("Signal lost");
+        return;
+        break;
+    case 0:
+        SBUS_Reader.print_data();
+        break;
+    }
 
-            MPU5060_Sensor.compute_orientation();
-            MPU5060_Sensor.compute_orientation();
+    Motor_Controller.update_mode(SBUS_Reader.get_mode());
 
-            sbus_status = SBUS_Reader.Read_Sbus();
-            // TODO handle safe mode
-            switch (sbus_status)
-            {
-            case 2:
-                Serial.println("Failsafe active");
-                return;
-                break;
-            case 1:
-                Serial.println("Signal lost");
-                sbus_status = SBUS_Reader.Read_Sbus();
-                // TODO handle safe mode
-                switch (sbus_status)
-                {
-                case 2:
-                    Serial.println("Failsafe active");
-                    return;
-                    break;
-                case 1:
-                    Serial.println("Signal lost");
-                    return;
-                    break;
-                case 0:
-                    SBUS_Reader.print_data();
-                    break;
-                    break;
-                case 0:
-                    SBUS_Reader.print_data();
-                    break;
-                }
+    Motor_Controller.update_motors(SBUS_Reader.get_data(), MPU5060_Sensor, dt);
 
-                Motor_Controller.update_mode(SBUS_Reader.get_mode());
+    Motor_Controller.set_vehicle_PWM();
 
-                Motor_Controller.update_motors(SBUS_Reader.get_data(), MPU5060_Sensor, dt);
-
-                Motor_Controller.set_vehicle_PWM();
-
-                SBUS_Reader.print_data();
-                // printing of PID data is in motor.cpp
-                MPU5060_Sensor.print_data();
-            }
+    SBUS_Reader.print_data();
+    // printing of PID data is in motor.cpp
+    MPU5060_Sensor.print_data();
+}
--- src/motor.cpp
+++ src/motor.cpp
@@ -33,100 +33,92 @@ void Motor_controller::reset_motors()
     motor_br.sendThrottle(0);
     motor_tl.sendThrottle(0);
     motor_tr.sendThrottle(0);
-    void Motor_controller::reset_motors()
-    {
-        motor_fl.sendThrottle(0);
-        motor_fr.sendThrottle(0);
-        motor_bl.sendThrottle(0);
-        motor_br.sendThrottle(0);
-        motor_tl.sendThrottle(0);
-        motor_tr.sendThrottle(0);
-    }
+}
 
-    void Motor_controller::update_mode(float change_to_)
+void Motor_controller::update_mode(float change_to_)
+{
+    if (change_to_ > 1000)
     {
-        if (change_to_ > 1000)
+        // Ensure all motors are stopped before switching modes
+        if (current_mode != MODE_COPTER && tl <= SWITCH_MOTOR_PWM_THRESHOLD && tr <= SWITCH_MOTOR_PWM_THRESHOLD)
         {
-            // Ensure all motors are stopped before switching modes
-            if (current_mode != MODE_COPTER && tl <= SWITCH_MOTOR_PWM_THRESHOLD && tr <= SWITCH_MOTOR_PWM_THRESHOLD)
-            {
-                current_mode = MODE_COPTER;
-                pids_inner.reset();
-                pids_outer.reset();
-            }
-        }
-        else
-        {
-            if (current_mode != MODE_TANK && bl <= SWITCH_MOTOR_PWM_THRESHOLD && br <= SWITCH_MOTOR_PWM_THRESHOLD && fl <= SWITCH_MOTOR_PWM_THRESHOLD && fr <= SWITCH_MOTOR_PWM_THRESHOLD)
-            {
-                current_mode = MODE_TANK;
-                pids_inner.reset();
-                pids_outer.reset();
-                // again place for servo
-            }
+            current_mode = MODE_COPTER;
+            pids_inner.reset();
+            pids_outer.reset();
         }
     }
-
-    void Motor_controller::update_motors(
-        float *sbus_data_,
-        Mpu6050_Sensor &mpu_sensor_,
-        float dt_)
+    else
     {
-        if (current_mode == MODE_TANK)
+        if (current_mode != MODE_TANK && bl <= SWITCH_MOTOR_PWM_THRESHOLD && br <= SWITCH_MOTOR_PWM_THRESHOLD && fl <= SWITCH_MOTOR_PWM_THRESHOLD && fr <= SWITCH_MOTOR_PWM_THRESHOLD)
         {
-            throttle_sp = sbus_data_[0] * THROTTLE_COEF_TANK;
-            yaw_sp = sbus_data_[1] * STEER_COEF;
-            pitch_sp = 0.0f;
-            roll_sp = 0.0f;
-
-            pid_yaw_ctrl = pids_inner.yaw.compute(mpu_sensor_.get_yaw_rate(), yaw_sp, dt_);
-            pid_roll_ctrl = pids_inner.roll.compute(mpu_sensor_.get_roll(), roll_sp, dt_);
-            pid_pitch_ctrl = pids_inner.pitch.compute(mpu_sensor_.get_pitch(), pitch_sp, dt_);
-
-            tl = throttle_sp - pid_yaw_ctrl + pid_roll_ctrl + pid_pitch_ctrl;
-            tr = throttle_sp + pid_yaw_ctrl - pid_roll_ctrl - pid_pitch_ctrl;
-
-            fl = 0;
-            fr = 0;
-            bl = 0;
-            br = 0;
+            current_mode = MODE_TANK;
+            pids_inner.reset();
+            pids_outer.reset();
+            // again place for servo
         }
-        else
-        {
-            throttle_sp = sbus_data_[0] * THROTTLE_COEF_COPTER;
-            yaw_sp = sbus_data_[1] * YAW_RATE_COEF;
-            pitch_sp = sbus_data_[2] * PITCH_COEF;
-            roll_sp = sbus_data_[4] * ROLL_COEF;
-
-            pid_yaw_ctrl = pids_inner.yaw.compute(mpu_sensor_.get_yaw_rate(), yaw_sp, dt_);
-            roll_desired = pids_outer.roll.compute(mpu_sensor_.get_roll(), roll_sp, dt_);
-            pitch_desired = pids_outer.pitch.compute(mpu_sensor_.get_pitch(), pitch_sp, dt_);
-            pid_roll_ctrl = pids_inner.roll.compute(mpu_sensor_.get_roll_rate(), roll_desired, dt_);
-            pid_pitch_ctrl = pids_inner.pitch.compute(mpu_sensor_.get_pitch_rate(), pitch_desired, dt_);
-
-            fr = throttle_sp - pid_yaw_ctrl + pid_roll_ctrl + pid_pitch_ctrl;
-            fl = throttle_sp + pid_yaw_ctrl - pid_roll_ctrl + pid_pitch_ctrl;
-            br = throttle_sp + pid_yaw_ctrl + pid_roll_ctrl - pid_pitch_ctrl;
-            bl = throttle_sp - pid_yaw_ctrl - pid_roll_ctrl - pid_pitch_ctrl;
-
-            tl = 0;
-            tr = 0;
-        }
-        // Print PID output
-        Serial.print("PID Control Yaw: ");
-        Serial.print(pid_yaw_ctrl);
-        Serial.print(", Roll: ");
-        Serial.print(pid_roll_ctrl);
-        Serial.print(", Pitch: ");
-        Serial.println(pid_pitch_ctrl);
     }
+}
 
-    void Motor_controller::set_vehicle_PWM()
+void Motor_controller::update_motors(
+    float *sbus_data_,
+    Mpu6050_Sensor &mpu_sensor_,
+    float dt_)
+{
+    if (current_mode == MODE_TANK)
     {
-        motor_fl.sendThrottle(fl);
-        motor_fr.sendThrottle(fr);
-        motor_bl.sendThrottle(bl);
-        motor_br.sendThrottle(br);
-        motor_tl.sendThrottle(tl);
-        motor_tr.sendThrottle(tr);
+        throttle_sp = sbus_data_[0] * THROTTLE_COEF_TANK;
+        yaw_sp = sbus_data_[1] * STEER_COEF;
+        pitch_sp = 0.0f;
+        roll_sp = 0.0f;
+
+        pid_yaw_ctrl = pids_inner.yaw.compute(mpu_sensor_.get_yaw_rate(), yaw_sp, dt_);
+        pid_roll_ctrl = pids_inner.roll.compute(mpu_sensor_.get_roll(), roll_sp, dt_);
+        pid_pitch_ctrl = pids_inner.pitch.compute(mpu_sensor_.get_pitch(), pitch_sp, dt_);
+
+        tl = throttle_sp - pid_yaw_ctrl + pid_roll_ctrl + pid_pitch_ctrl;
+        tr = throttle_sp + pid_yaw_ctrl - pid_roll_ctrl - pid_pitch_ctrl;
+
+        fl = 0;
+        fr = 0;
+        bl = 0;
+        br = 0;
     }
+    else
+    {
+        throttle_sp = sbus_data_[0] * THROTTLE_COEF_COPTER;
+        yaw_sp = sbus_data_[1] * YAW_RATE_COEF;
+        pitch_sp = sbus_data_[2] * PITCH_COEF;
+        roll_sp = sbus_data_[4] * ROLL_COEF;
+
+        pid_yaw_ctrl = pids_inner.yaw.compute(mpu_sensor_.get_yaw_rate(), yaw_sp, dt_);
+        roll_desired = pids_outer.roll.compute(mpu_sensor_.get_roll(), roll_sp, dt_);
+        pitch_desired = pids_outer.pitch.compute(mpu_sensor_.get_pitch(), pitch_sp, dt_);
+        pid_roll_ctrl = pids_inner.roll.compute(mpu_sensor_.get_roll_rate(), roll_desired, dt_);
+        pid_pitch_ctrl = pids_inner.pitch.compute(mpu_sensor_.get_pitch_rate(), pitch_desired, dt_);
+
+        fr = throttle_sp - pid_yaw_ctrl + pid_roll_ctrl + pid_pitch_ctrl;
+        fl = throttle_sp + pid_yaw_ctrl - pid_roll_ctrl + pid_pitch_ctrl;
+        br = throttle_sp + pid_yaw_ctrl + pid_roll_ctrl - pid_pitch_ctrl;
+        bl = throttle_sp - pid_yaw_ctrl - pid_roll_ctrl - pid_pitch_ctrl;
+
+        tl = 0;
+        tr = 0;
+    }
+    // Print PID output
+    Serial.print("PID Control Yaw: ");
+    Serial.print(pid_yaw_ctrl);
+    Serial.print(", Roll: ");
+    Serial.print(pid_roll_ctrl);
+    Serial.print(", Pitch: ");
+    Serial.println(pid_pitch_ctrl);
+}
+
+void Motor_controller::set_vehicle_PWM()
+{
+    motor_fl.sendThrottle(fl);
+    motor_fr.sendThrottle(fr);
+    motor_bl.sendThrottle(bl);
+    motor_br.sendThrottle(br);
+    motor_tl.sendThrottle(tl);
+    motor_tr.sendThrottle(tr);
+}
